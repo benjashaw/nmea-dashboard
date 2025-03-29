0f2413311ad20d4df48a87c6d40fde40b3aefbdf
benjashaw <benjaminshaw@gmail.com>
2025-03-29 06:26:52 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+data_store = {}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/api/nmea")
+async def receive_nmea(req: Request):
+    incoming = await req.json()
+    data_store.update(incoming)
+    return {"status": "ok"}
+
+@app.get("/api/data")
+def get_nmea_data():
+    return data_store
